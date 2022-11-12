3f067091a4bf77728e6c3b1142cdc9ae227bae15
nattofriends <nattofriends@users.noreply.github.com>
2022-11-11 22:10:55 -0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -8,26 +8,7 @@ set -o pipefail
 mkdir "poppler-$POPPLER_VERSION"
 cd "poppler-$POPPLER_VERSION" || exit
 
-cp -a "$PKGS_PATH_DIR"/poppler-$POPPLER_VERSION*/Library/ .
-cp "$PKGS_PATH_DIR"/freetype*/Library/bin/freetype.dll ./Library/bin/
-cp "$PKGS_PATH_DIR"/libzlib*/Library/bin/zlib.dll ./Library/bin/
-cp -a "$PKGS_PATH_DIR"/zstd*/Library/bin/. ./Library/bin/
-cp "$PKGS_PATH_DIR"/libtiff*/Library/bin/tiff.dll ./Library/bin/
-cp "$PKGS_PATH_DIR"/libtiff*/Library/bin/libtiff.dll ./Library/bin/
-cp "$PKGS_PATH_DIR"/libssh2*/Library/bin/libssh2.dll ./Library/bin/
-cp "$PKGS_PATH_DIR"/libpng*/Library/bin/libpng16.dll ./Library/bin/
-cp "$PKGS_PATH_DIR"/libcurl*/Library/bin/libcurl.dll ./Library/bin/
-cp "$PKGS_PATH_DIR"/openssl*/Library/bin/libcrypto-3-x64.dll ./Library/bin/
-cp "$PKGS_PATH_DIR"/openjpeg*/Library/bin/openjp2.dll ./Library/bin/
-cp "$PKGS_PATH_DIR"/xz*/Library/bin/liblzma.dll ./Library/bin/
-cp "$PKGS_PATH_DIR"/cairo*/Library/bin/cairo.dll ./Library/bin/
-cp "$PKGS_PATH_DIR"/libdeflate*/Library/bin/libdeflate.dll ./Library/bin/
-cp "$PKGS_PATH_DIR"/lerc*/Library/bin/Lerc.dll ./Library/bin/
-cp "$PKGS_PATH_DIR"/jbig*/Library/bin/jbig.dll ./Library/bin/
-cp "$PKGS_PATH_DIR"/lcms2*/Library/bin/lcms2.dll ./Library/bin/
-cp "$PKGS_PATH_DIR"/fontconfig*/Library/bin/fontconfig-1.dll ./Library/bin/
-cp "$PKGS_PATH_DIR"/expat*/Library/bin/libexpat.dll ./Library/bin/
-cp -a "$PKGS_PATH_DIR"/libiconv*/Library/bin/. ./Library/bin/
+find "$PKGS_PATH_DIR" -f -name "*.dll" -exec cp ./Library/bin {} \;
 
 rm -rf "$PKGS_PATH_DIR"
 
